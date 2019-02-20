f79b4151ee1f7f00585f0850c8b8238e07b3bca3
Alex <awest36@rocketmail.com>
2019-02-20 12:07:21 -0600

    updating
    debugging the software

--- greetName/ViewController.swift
+++ greetName/ViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var messageText: UILabel!
+    @IBOutlet weak var greetbutton: UIButton!
+    @IBAction func greetClicked(_ sender: Any) {
+        greetbutton.setTitleColor( .green, for: .normal)
+        messageText.text = "hello, " + textField.text! + "!"
+        textField.text = ""
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
